eb7907a3b6e4bf08e3407de7ed7a85edcd8a533b
Victoria045 <victoriaberyl45@gmail.com>
2021-10-15 17:18:20 +0300

    create the class access modifiers

--- src/app/user-class/user.ts
+++ src/app/user-class/user.ts
@@ -1,9 +1,11 @@
 export class User {
-      created_at?: string;
-      avatar_url: any;
-      followers_url?: number;
-      following_url?: number;
-      repos_url?: string;
-      login?: number;
-      public_repos?: string;
+
+  constructor(public bio: string, public public_repos: number, public login:string, public avatar_url:string, public created_at:Date, public updated_at:Date, public html_url: string){}
+      // created_at?: string;
+      // avatar_url: any;
+      // followers_url?: number;
+      // following_url?: number;
+      // repos_url?: string;
+      // login?: number;
+      // public_repos?: string;
 }
