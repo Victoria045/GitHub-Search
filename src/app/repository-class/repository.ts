5b565784f20bf0d5dbf7a610f295894678994572
Victoria045 <victoriaberyl45@gmail.com>
2021-10-15 17:15:53 +0300

    create the constructor access modifiers

--- src/app/repository-class/repository.ts
+++ src/app/repository-class/repository.ts
@@ -1,6 +1,8 @@
 export class Repository {
-    id?: string;
-    name?: string;
-    html_url?: string;
-    description?: string;
+
+  constructor(public name: string, public full_name: string, public description: string, public updated_at:Date, public html_url: string, public clone_url: string, public language: string, public created_at:Date){}
+    // id?: string;
+    // name?: string;
+    // html_url?: string;
+    // description?: string;
 }
