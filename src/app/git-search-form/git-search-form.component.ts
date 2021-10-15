90cb9cb38e5a16d04907a3fa3ab16376ef027f10
Victoria045 <victoriaberyl45@gmail.com>
2021-10-15 17:14:03 +0300

    import files and create methods

--- src/app/git-search-form/git-search-form.component.ts
+++ src/app/git-search-form/git-search-form.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { GitSearchService } from '../services/git-search.service';
+import { User } from '../user-class/user';
 
 @Component({
   selector: 'app-git-search-form',
   templateUrl: './git-search-form.component.html',
   styleUrls: ['./git-search-form.component.css']
 })
+
 export class GitSearchFormComponent implements OnInit {
 
-  constructor() { }
+  user?: User;
+  username?:string;
+  gitSearchService: GitSearchService;
+  
 
+  searchUser() {
+    this.gitSearchService.getUserData(this.username as string);
+  }
+
+  constructor(gitSearchService: GitSearchService) { 
+    this.gitSearchService = gitSearchService;
+  }
+
+  
   ngOnInit(): void {
   }
 
