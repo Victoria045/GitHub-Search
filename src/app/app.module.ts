d30cf95fd82e0205badeee0ab75729cb46f4ca0b
Victoria045 <victoriaberyl45@gmail.com>
2021-10-15 06:16:00 +0300

    import the needed files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { GitSearchFormComponent } from './git-search-form/git-search-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    GitSearchFormComponent
   ],
   imports: [
     BrowserModule,
