f43720ecd3328ce206b6fa6c1f3712f30f673f87
Victoria045 <victoriaberyl45@gmail.com>
2021-10-17 22:01:26 +0300

    import routing path component files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GitSearchFormComponent } from './git-search-form/git-search-form.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { OutputComponent } from './output/output.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:GitSearchFormComponent},
+  { path: 'output', component:OutputComponent },
+  { path: '', redirectTo:'/output', pathMatch:"full"},
+  { path:'**', component:NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
