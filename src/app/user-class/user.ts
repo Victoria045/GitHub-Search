4419e69a4b81b977de4c541e59346c30efe722a0
Victoria045 <victoriaberyl45@gmail.com>
2021-10-17 22:08:55 +0300

    update the constructor

--- src/app/user-class/user.ts
+++ src/app/user-class/user.ts
@@ -1,6 +1,6 @@
 export class User {
 
   constructor(public bio: string, public public_repos: number, public login:string, public avatar_url:string, public created_at:Date, public updated_at:Date, public html_url: string,
-    public followers: number, public following: number){}
+    public company: string,public location: string,public email: string,public followers: number, public following: number){}
   
 }
