73cbb871414a743b848e296cb7167a10db56a478
Victoria045 <victoriaberyl45@gmail.com>
2021-10-18 07:43:44 +0300

    update searchUser method

--- src/app/git-search-form/git-search-form.component.ts
+++ src/app/git-search-form/git-search-form.component.ts
@@ -24,7 +24,6 @@ export class GitSearchFormComponent implements OnInit {
   }
 
   searchUser() {
-    // this.loading = false;
     this.gitSearchService.getUserData(this.username as string);
     this.router.navigate(['/output']);
     
